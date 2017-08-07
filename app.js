06c2097d5963b77383817baed9073e021df947c1
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2017-08-07 12:02:18 +0300

    Fixes

--- app.js
+++ app.js
@@ -175,16 +175,16 @@ app.service('studentService', ['$q', function ($q) {
         },
         {
             "name": "Yurii Petrichenko",
-            "websiteUrl": "https://madebyspeedster.github.io/monday/",
-            "codeSourceUrl": "https://github.com/madebyspeedster/monday/",
+            "websiteUrl": "https://madebyspeedster.github.io/pr1/",
+            "codeSourceUrl": "https://github.com/madebyspeedster/pr1/",
             "cvUrl": "",
             "photo": "images/students/petrichenko.jpg"
         },
         {
             "name": "Nazar Kyrychenko",
-            "websiteUrl": "https://weindor.github.io/landing-page/",
-            "codeSourceUrl": "",
-            "cvUrl": "",
+            "websiteUrl": "https://weindor.github.io/projectinspirationtocode/",
+            "codeSourceUrl": "https://github.com/weindor/projectinspirationtocode/",
+            "cvUrl": "https://www.linkedin.com/in/%D0%BD%D0%B0%D0%B7%D0%B0%D1%80-%D0%BA%D0%B8%D1%80%D0%B8%D1%87%D0%B5%D0%BD%D0%BA%D0%BE-22b00811a/",
             "photo": "images/students/kyrychenko.jpg"
         },
         {
